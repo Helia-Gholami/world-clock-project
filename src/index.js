bc3c9b8a74d7bf7e6209f177901579df6da60774
Helia-Gholami <gheliagholami@gmail.com>
2025-05-22 00:18:05 +0330

    added js to the project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-
+let losAngelesElement = document.querySelector("#los-angeles");
+let losAngelesDateElement = losAngelesElement.querySelector(".date");
+let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+let losAngelesTime = moment().tz(America / Los - angeles);
